8f7ae5c14a3835c0b1eb82b77f1786a422c94916
Ariszy <zhiyi8028@gmail.com>
2021-10-20 15:45:52 +0800

    更新用户数据

--- ccfxj.js
+++ ccfxj.js
@@ -1,1 +1,1 @@
-null
+{"1419395568":{"RtGKtY7NOlDDIPvrU7VimsxUm9mnBiKFnMQqFX7KpER4NJ4t":{"Code":"RtGKtY7NOlDDIPvrU7VimsxUm9mnBiKFnMQqFX7KpER4NJ4t","Date":1634715940,"Name":"Ariszy","Uid":1419395568}}}
